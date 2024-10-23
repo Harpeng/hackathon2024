8709d7a96a1d2bf9e714c4331a54fceff6be1242
Harpeng <gondarev75@gmail.com>
2024-10-23 23:22:34 +0300

    feat(project): initial setting project

---
+++ .eslintrc.cjs
@@ -0,0 +1,94 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT'
+        ],
+        alphabetical: false
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 3
+        },
+        multiline: {
+          max: 1
+        }
+      }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    'vue/no-dupe-keys': [
+      'error',
+      {
+        groups: []
+      }
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'always',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ],
+    semi: true,
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'arrow-parens': ['error', 'as-needed'],
+    'no-unused-expressions': 'error',
+    'no-param-reassign': 'off',
+    'no-unused-vars': 'error',
+    'max-len': ['error', { code: 120 }],
+  }
+}
